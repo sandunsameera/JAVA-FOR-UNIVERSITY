751f0f9a2b9d4e056767142f3c0ed84bc1d5d8fd
sandunsameera <sandunsameera25@gmail.com>
2019-01-02 14:52:08 +0530

    do while loop

---
+++ loop.java
@@ -0,0 +1,12 @@
+public class loop{
+    public static void main(String[] args){
+        int counter = 0;
+
+        do{
+            System.out.println(counter);
+            counter = counter+1;
+        }
+
+        while(counter!=5);
+    }
+}
