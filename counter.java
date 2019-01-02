748b2e73a7addc7537232a9f7762bec90889f05d
sandunsameera <sandunsameera25@>
2019-01-02 18:34:01 +0530

    program to calculate sum and avg of numbers

---
+++ counter.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class counter{
+
+    public static void main(String[] args){
+        int number;
+        int sum=0;
+        int avg;
+        int i=0;
+        int n;
+
+        System.out.println("Enter amount of numbers you use: ");
+        Scanner input = new Scanner(System.in);
+        n = input.nextInt();
+
+
+        while(i<n){
+            System.out.println("Enter your number ");
+            Scanner num = new Scanner(System.in);
+            number = num.nextInt();
+            sum = sum + number;
+            i++;
+            avg = sum/i;
+            System.out.println("your sum :  " + sum);
+            System.out.println("your average :  " + avg);
+
+
+        }
+    }
+
+
+}
