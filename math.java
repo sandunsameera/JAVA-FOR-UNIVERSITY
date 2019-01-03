9d8ae765bf17a5f6c6216661d8361134be550712
sandunsameera <sandunsameera25@gmail.com>
2019-01-03 13:12:29 +0530

    math functions

---
+++ math.java
@@ -0,0 +1,9 @@
+public class math{
+    public static void main(String[] args){
+        System.out.println(Math.pow(3,3));
+        System.out.println(Math.ceil(3.44));
+        System.out.println(Math.max(3,8));
+        System.out.println(Math.min(3,3));
+        System.out.println(Math.sqrt(9));
+    }
+}
