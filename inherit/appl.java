02ed6260fd8ba14ab2deacb4d1c294c69b8e2d8d
sandunsameera <sandunsameera25@gmail.com>
2019-01-08 15:50:19 +0530

    use of inherit

---
+++ inherit/appl.java
@@ -0,0 +1,5 @@
+public class appl{
+    public void fruits(){
+        System.out.println("Im the fruit method..");
+    }
+}
---
+++ inherit/fruit.java
@@ -0,0 +1,9 @@
+public class fruit{
+    public static void main(String[] args) {
+        orang  orangObject = new orang();
+        grapes  grapesObject = new grapes();
+
+        orangObject.fruits();
+        grapesObject.fruits();
+    }
+}
