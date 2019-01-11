54cb0f45f5d635d701fee79527804bb4f86bc2d6
sandunsameera <sandunsameera25@gmail.com>
2019-01-11 11:17:56 +0530

    Application using inheritance and pholimorphism

---
+++ applicationUsingInheritanceAndPholimorphism/abc.java
@@ -0,0 +1,7 @@
+public class abc{
+    public static void main(String[] args) {
+        dogList DLO = new dogList();
+        dog d = new dog();
+        DLO.add(d);
+    }
+}
--- pholimorphism/ac.java
+++ pholimorphism/ac.java
@@ -10,4 +10,4 @@ public class ac{
         sandun.digest(vigiObject);
 
     }
-}
+} 
